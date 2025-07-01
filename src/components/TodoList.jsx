5e60d2d8f794aa11ac455d084968d85f0126e5b1
Daniil10Sh <danik2022odessa@gmail.com>
2025-07-01 19:54:49 +0300

    changed function into class

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 export default class TodoList extends React.Component {
     render() {
         return (
@@ -10,6 +11,7 @@ export default class TodoList extends React.Component {
                     <li>Deploy the App</li>
                 </ul>
             </div>
+        
         );
     }
 }
