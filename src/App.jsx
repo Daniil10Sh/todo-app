98fabf00822115697b23d4f422028dd97b397103
Daniil10Sh <danik2022odessa@gmail.com>
2025-07-09 18:04:14 +0300

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,60 @@
 import "./App.css";
-import React from "react";
 import TodoList from "./components/TodoList.jsx";
 import initialTodos from "./todo.json";
+import { Component } from "react";
+import TodoEditor from "./components/TodoEditor.jsx";
+import Filter from "./components/Filter.jsx";
 
-class App extends React.Component {
+class App extends Component {
   state = {
     todos: initialTodos,
     filter: "",
   };
+
+  addTodo = (text) => {
+    const newTodo = {
+      id: crypto.randomUUID(),
+      text,
+      completed: false,
+    };
+    this.setState((prevState) => ({
+      todos: [...prevState.todos, newTodo],
+    }));
+  };
+
+  onToggleCompleted = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    }));
+  };
+  onDelete = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => todo.id !== id),
+    }))
+  };
+  handleFilter = (value) => {
+    this.setState({ filter: value })
+  }
+
+  getFilteredTodos = () => {
+    const {  todos, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return todos.filter((todo) => todo.text.toLowerCase().includes(normalizedFilter) )
+  };
   render() {
     return (
       <>
-        <TodoList todos={this.state.todos} />
+        <TodoEditor onSubmit={this.addTodo} />
+        <TodoList
+          todos={this.state.todos}
+          onToggleCompleted={this.onToggleCompleted}
+          onDelete={this.onDelete}
+ 
+        />
+
+        <Filter onFilterChange={this.handleFilter} value={this.state.filter} />
       </>
     );
   }
